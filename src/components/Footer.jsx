ef63cf2a3de0cb44f846c9098f591e3e2dd97bd0
ARTEMII PYLYPENKO <artemii261204@gmail.com>
2025-11-05 14:22:23 +0200

    Lab10_2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer(){
     return (
         <footer className="footer">
             <div className="footer__inner">
-                <p className="footer__brand">© {currentYear} Lab 10 by Kryvyi Yaroslav</p>
+                <p className="footer__brand">© {currentYear} Lab 10 by Pylypenko Artemiy</p>
                 <p>Побудовано на Vite + React</p>
             </div>
         </footer>
